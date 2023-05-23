c436dcdfad4e494d601677225b85f9de96a6a9ad
01mk027 <melektavusintikam@gmail.com>
2023-05-23 17:54:31 +0300

    ss

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using WebApplication1.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => 
+    policy.AllowAnyMethod()
+          .AllowAnyHeader()
+          .AllowCredentials()
+          .SetIsOriginAllowed(origin => true)
+));
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+
+app.UseCors();
+
+
+app.MapHub<MyHub>("/myhub");
+
+app.Run();
